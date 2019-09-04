ce0cf408fd18610b34cebbd6936ca04d25c5f69b
Brandon Evans <brandon_n_evans@live.com>
2019-09-03 20:09:18 -0500

    add second practice app

---
+++ Nuxt/app-two/pages/products/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This will show a single product with the id of: {{$route.params.id}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Nuxt/app-two/pages/products/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>This is the products page</h1>
+        <nuxt-link to="/products/1">Go to first product</nuxt-link>    
+        <nuxt-link to="/products/2">Go to second product</nuxt-link>  
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
