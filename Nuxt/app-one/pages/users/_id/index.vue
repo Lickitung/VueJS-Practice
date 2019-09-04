ac804ec1a6950a2f9ca37e631ad8bb16dd152b58
SIeep <brandon_n_evans@live.com>
2019-09-04 08:59:11 -0500

    add validation

--- Nuxt/app-one/pages/users/_id/index.vue
+++ Nuxt/app-one/pages/users/_id/index.vue
@@ -3,3 +3,12 @@
         <h1>A single User with ID: {{$route.params.id}}</h1>
     </div>
 </template>
+
+<script>
+export default {
+    validate (data) {
+        console.log(data)
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
