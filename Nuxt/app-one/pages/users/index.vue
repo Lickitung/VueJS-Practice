3ea2ea9e44849214d36ca16a7c00079e18ca0e8b
SIeep <brandon_n_evans@live.com>
2019-09-04 09:07:43 -0500

    add nested route

--- Nuxt/app-one/pages/users/index.vue
+++ Nuxt/app-one/pages/users/index.vue
@@ -1,26 +1,7 @@
 <template>
     <div>
-        <h1>This is the users page</h1>
-        <input type="text" v-model="userId">
-        <button @click="onLoadUser">Load User</button>
+        <p>Please enter an id and click the button</p>
     </div>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-                userId: ''
-            }
-        },
-        methods: {
-            onLoadUser() {
-                this.$router.push('/users/' + this.userId)
-            }
-        }
-    }
-</script>
 
-<style lang="scss" scoped>
-
-</style>
